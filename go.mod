145c004e5bc49a243aa3acce904eca67d2b03f8f
ShenNaizhi <2581125676@qq.com>
2026-09-14 19:50:05 +0800

    Init the go module. Implement a simple work flow of scanning folders.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/gogitlocalstats
+
+go 1.26.4
--- src/main.go
+++ src/main.go
@@ -1,14 +1,74 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"os"
+	"log"
+)
 
-// when given a path, scan crawls it and its subfolders
+// when given a path, scan() crawls it and its subfolders
 // searching for Git repositories
-func scan(path string) {
-	print("scan")
+func scan(folder string) {
+	fmt.Printf("Found folders:\n\n")
+
+	repositories := recursiveScanFolder(folder) // get a slice of strings
+	filePath := getDotFilePath() // get the path of the dot file for output
+	addNewSliceElementsToFile(filePath, repositories) // output the slice contents to dot file
+
+	fmt.Printf("\n\nSuccessfully added\n\n")
 }
 
-// stats generates a graph of the local Git contributions
+// scanGitFolders() returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the `.git` folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func scanGitFolders(folders []string, folder string) []string {
+	// trim the last "/"
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	files, err := f.ReadDir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := ""
+	toSkip := map[string]bool {
+		"vendor": true,
+		"node_modules": true,
+	}
+	for _, file := range files {
+		// only check dirs
+		if !file.IsDir() {
+			continue
+		}
+
+		fileName := file.Name()
+
+		if fileName == ".git" {
+			fmt.Println(path)
+			folders = append(folders, path)
+			continue
+		}
+
+		if toSkip[fileName] {
+			continue
+		}
+
+		path = path + "/" + fileName
+		folders = scanGitFolders(folders, path)
+	}
+
+	return folders
+}
+
+// stats() generates a graph of the local Git contributions
 func stats(email string) {
 	print("stats")
 }
