efca6d418247a7df5ac9080287520c3ef4c22a26
ShenNaizhi <2581125676@qq.com>
2026-09-24 17:36:02 +0800

    Completed the whole project!

--- src/stats.go
+++ src/stats.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 	"sort"
+	"slices"
 	"strings"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
@@ -12,6 +13,7 @@ import (
 type column []int
 
 const lookbackDays = 183
+var currentTime time.Time = time.Now()
 
 // stats() generates a graph of the local Git contributions
 func stats(email string) {
@@ -26,7 +28,7 @@ func processRepos(email string) map[int]int {
 	repos := parseFileLinesToSlice(filePath) // parse all lines of the file in a slice
 
 	res := make(map[int]int, lookbackDays)
-	for i := lookbackDays; i > 0; i-- { // initialize the commits map
+	for i := 0; i < lookbackDays; i++ { // initialize the commits map
 		res[i] = 0
 	}
 
@@ -59,13 +61,13 @@ func fillCommits(email string, path string, commits map[int]int) map[int]int {
 	}
 
 	// iterate on commits
-	offset := weekdayOffset(time.Now())
+	offset := weekdayOffset(currentTime)
 	err = iter.ForEach(func (c *object.Commit) error {
 		if c.Author.Email != email { // not target user
 			return nil
 		}
 
-		days := daysBetween(c.Author.When, time.Now())
+		days := daysBetween(c.Author.When, currentTime)
 		if days <= lookbackDays {
 			commits[days + offset]++
 		}
@@ -100,13 +102,13 @@ func normalizeToUTCMidnight(t time.Time) time.Time {
 	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
 }
 
-// Determines the weekday represented by `t` [0, 7).
+// Make up the last week.
 // Adapted from `calcOffset()` in the original tutorial.
 func weekdayOffset(t time.Time) int {
 	res := int(t.Weekday()) // American standard
-	res = res % 7 + 1 // ISO 8601: Monday is the first day of a week
+	res = (res - 1 + 7) % 7 // ISO 8601: Monday is the first day of a week
 
-	return res
+	return 7 - (res + 1)
 }
 
 // Render the git commit stats in terminal.
@@ -134,15 +136,16 @@ func buildCols(keys []int, commits map[int]int) map[int]column {
 
 	for _, k := range keys {
 		week := k / 7
-		weekday := k % 7
+		weekday := 6 - k % 7
 
-		if weekday == 0 { // the first day of a week
+		if weekday == 6 { // the last day of a week
 			col = column{} // reset
 		}
 
 		col = append(col, commits[k])
 
-		if week == 6 { // the last day of a week
+		if weekday == 0 { // the first day of a week
+			slices.Reverse(col)
 			res[week] = col // complete a column
 		}
 	}
@@ -154,16 +157,17 @@ func buildCols(keys []int, commits map[int]int) map[int]column {
 func printCells(cols map[int]column) {
 	printMonths() // head of column
 
-	totalWeeks := (lookbackDays - 1) / 7 + 1 // ceiling
-	offset := weekdayOffset(time.Now())
+	offset := weekdayOffset(currentTime)
+	totalWeeks := (lookbackDays + offset - 1) / 7 + 1 // ceiling
 	for wd := 0; wd < 7; wd++ { // wd: weekday
 		for w := totalWeeks; w >= 0; w-- { // w: current week index [0, totalWeeks)
 			if w == totalWeeks { // head of row
 				printWeekday(wd)
+				continue
 			}
 
 			if c, ok := cols[w]; ok { // c: current column
-				if w == 0 && wd == offset { // today
+				if w == 0 && wd == 6 - offset { // today
 					printOneCell(c[wd], true)
 				} else if len(c) > wd { // other dates
 					printOneCell(c[wd], false)
@@ -178,9 +182,9 @@ func printCells(cols map[int]column) {
 
 // Renders the month names in the first line of the whole stat graph.
 func printMonths() {
-	week := normalizeToUTCMidnight(time.Now()).AddDate(0, 0, -lookbackDays) // during a certain week
+	week := normalizeToUTCMidnight(currentTime).AddDate(0, 0, -lookbackDays) // during a certain week
 	month := week.Month()
-	cur := time.Now()
+	cur := currentTime
 
 	fmt.Print(strings.Repeat(" ", 9))
 	// fmt.Printf("%s ", week.Month().String()[:3]) // abbreviation of weekdays
@@ -215,7 +219,7 @@ func inSameWeek(a, b time.Time) bool {
 		return false // not in the same week
 	}
 
-	return weekdayOffset(a) < weekdayOffset(b)
+	return weekdayOffset(a) >= weekdayOffset(b)
 }
 
 // Prints the abbreviation of the weekday.
